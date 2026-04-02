1aa0e2da63c6888ca5db8b7abfaeabe5a577e919
tfnlxxxsilent-cloud <tfnlxxxsilent@gmail.com>
2026-04-02 23:54:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCApplication.hpp>
+#include <cocos2d.h>
 
 using namespace geode::prelude;
+using namespace cocos2d;
 
-class $modify(CCApplication) {
+class $modify(MyCCApplication, CCApplication) {
     void setAnimationInterval(double interval) {
-        int targetFps = Mod::get()->getSettingValue<int64_t>("target-fps");
-        
+        int targetFps = static_cast<int>(Mod::get()->getSettingValue<int64_t>("target-fps"));
+
         if (targetFps > 0) {
-            CCApplication::setAnimationInterval(1.0 / targetFps);
+            CCApplication::setAnimationInterval(1.0 / static_cast<double>(targetFps));
+            log::info("Set FPS to {}", targetFps);
         } else {
             CCApplication::setAnimationInterval(interval);
         }
